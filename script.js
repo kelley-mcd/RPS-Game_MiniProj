eea5c5c0ea50c007effc943d8db4e845ac3bd799
kelley-mcd <kmcdonnell8@gmail.com>
2021-06-26 13:03:13 -0400

    commit

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 
 //alert()`, `confirm()`, and `prompt()`
 
+let gameStart = "Let's play a game!";
+let gamePrompt = "Choose your player: R, P, or S";
+let playQuestion = "Do you want to play again?";
+let rock = "you chose rock";
+
+if (window.prompt() === "R") {
+    return rock;
+}
+
+
+
+window.alert(gameStart);
+window.prompt(gamePrompt);
+window.confirm(playQuestion);
