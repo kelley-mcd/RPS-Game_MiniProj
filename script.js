d3c2db1a76fc8c7eb489c328736a5d00f399e1bf
kelley-mcd <kmcdonnell8@gmail.com>
2021-06-26 13:40:55 -0400

    commit

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 //rock paper siz game
 
 //alert()`, `confirm()`, and `prompt()`
+let wins = 0;
+let ties = 0;
+let loses = 0;
 
-let gameStart = "Let's play a game!";
-let gamePrompt = "Choose your player: R, P, or S";
-let playQuestion = "Do you want to play again?";
-let rock = "you chose rock";
+let options = ["R", "P", "S"];
 
-if (window.prompt() === "R") {
-    return rock;
-}
+let playGame = function () {
+
+    let userChoice = window.prompt("Choose your player: R, P, or S");
+
+    if (!userChoice) {
+        return;
+    }
+
+    userChoice = userChoice.toUpperCase();
+
+    let index = Math.floor(Math.random() * options.length);
+    let computerChoice = options[index];
 
 
+    if (userChoice === computerChoice) {
+        ties++;
+        window.alert("it's a tie");
+    }
+    else if (
+        (userChoice === "R" && computerChoice === "S") ||
+        (userChoice === "P" && computerChoice === "R") ||
+        (userChoice === "S" && computerChoice === "P")
+    ) {
+        wins++;
+        window.alert("You win");
+    }
+    else {
+        losses++;
+        window.alert("you lost");
+    }
 
-window.alert(gameStart);
-window.prompt(gamePrompt);
-window.confirm(playQuestion);
+    window.alert(
+        "Stats:\nWins:" + wins + "\nLosses:" + losses + "\nTies:" + ties
+    );
+
+    if (playAgain) {
+        playGame();
+    }
+};
+
+playGame();
